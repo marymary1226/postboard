86d3c85f3e5d5d5c98c1d85d6342f67f6ddd4a28
marymary1226 <marymary1226@gmail.com>
2015-12-13 14:38:59 +0800

    push to Heroku

---
+++ app/controllers/concerns/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ActionController::Base
+
+  def index
+    @posts = Post.all
+  end
+
+  
+  def show
+    @post = Post.find(params[:id])
+  end
+
+end
--- db/migrate/20151209142342_add_category_table.rb
+++ db/migrate/20151209142342_add_category_table.rb
@@ -5,6 +5,7 @@ class AddCategoryTable < ActiveRecord::Migration
 
      t.timestamps
     end
+    
     create_table :categories_posts do |t|
      t.integer :post_id
      t.integer :category_id
